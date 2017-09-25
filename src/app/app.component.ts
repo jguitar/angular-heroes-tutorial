b0a71a7ef0f1d4d18ebcba819abf92db5299ffbd
Juan F. Pérez <j.guitar@gmail.com>
2017-09-25 11:25:06 +0200

    service returns a promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,6 +82,6 @@ export class AppComponent implements OnInit  {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
